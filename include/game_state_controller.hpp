104bf4803dcf5b6f35dfe930fa3358baf84f6d2f
RcrossF <finnmorin@gmail.com>
2022-07-31 13:17:36 -0700

    Split controller into UI and game state, switch from GLUT to glfw3

--- include/game_state_controller.hpp
+++ include/game_state_controller.hpp
@@ -1,101 +1,130 @@
 #pragma once
+#include <stdlib.h>
+#include <stdio.h>
 #include <GL/glew.h>
-#include <GL/glut.h>
 #include <GL/glu.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 #include <string>
 #include <exception>
+#include <chrono>
 #include "point_manager.hpp"
 
+#define PIXEL_FORMAT GL_RGB
 #define WIDTH 640
 #define HEIGHT 480
 #define FPS 60
 
 namespace game {
-    class GameStateController {
+    class UIController {
         public:
-            // Create empty point manager and initialize openGL window
-            GameStateController(int argc, char* argv[]): point_manager(PointManager()) {
+            UIController(int argc, char* argv[]): window(nullptr), ratio(0.0f) {
                 try {
-                    initGL(argc, argv);
+                    initGLFW();
                 } catch (std::exception& e) {
                     std::cout << "Failed to initialize OpenGL" << std::endl;
                     exit(1);
                 }
+            }
 
+            ~UIController() {
+                glfwTerminate();
+            }
+
+            static void error_callback(int error, const char* description) {
+                std::cout << "GLFW Error: " << description << std::endl;
+            }
+            static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+                if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+                    glfwSetWindowShouldClose(window, GL_TRUE);
+            }
+
+            void render() {
+                glClear(GL_COLOR_BUFFER_BIT);
+                int width, height;
+                glfwGetFramebufferSize(window, &width, &height);
+                ratio = width / (float)height;
+                glViewport(0, 0, width, height);
+                glMatrixMode(GL_PROJECTION);
+                glLoadIdentity();
+                glMatrixMode(GL_MODELVIEW);
+                glLoadIdentity();
+                glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+                glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+                glBegin(GL_TRIANGLES);
+                glColor3f(1.f, 0.f, 0.f);
+                glVertex3f(-0.6f, -0.4f, 0.f);
+                glColor3f(0.f, 1.f, 0.f);
+                glVertex3f(0.6f, -0.4f, 0.f);
+                glColor3f(0.f, 0.f, 1.f);
+                glVertex3f(0.f, 0.6f, 0.f);
+                glEnd();
+                glfwSwapBuffers(window);
+            }
+
+            void initGLFW() {
+                glfwSetErrorCallback(error_callback);
+                if (!glfwInit())
+                    throw std::runtime_error("Failed to initialize GLFW");
+                
+                int major, minor, rev;
+                glfwGetVersion(&major, &minor, &rev);
+                std::cout << "glfw major.minor " << major << "." << minor << "." << rev << std::endl;
+
+                window = glfwCreateWindow(HEIGHT, WIDTH, "Hello World", NULL, NULL);
+                if (!window) {
+                    glfwTerminate();
+                    throw std::runtime_error("Failed to create window");
+                }
+
+                glfwMakeContextCurrent(window);
+                glfwSetKeyCallback(window, key_callback);
+            }
+
+            // Should close the window?
+            bool shouldClose() {
+                return glfwWindowShouldClose(window);
+            }
+
+        private:
+            GLFWwindow* window;
+            float ratio;
+    };
+
+    class GameStateController {
+        public:
+            // Create empty point manager and initialize openGL window
+            GameStateController(int argc, char* argv[]): point_manager(PointManager()), ui_controller(UIController(argc, argv)) {
+                // Start GLUT main loop
+                // glutTimerFunc( 1000 / FPS, static_loop, 0 );
+                // glutMainLoop();
+                main_loop();
             }
             ~GameStateController() = default;
             
         private:
-            void initGL(int argc, char* argv[]) {
-                glutInit(&argc, argv);
-                glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-                glutInitWindowSize(WIDTH, HEIGHT);
-                glutInitWindowPosition(100, 100);
-                glutCreateWindow("Earthquake Simulator");
-                glutDisplayFunc(render);
-                glutIdleFunc(update_game_state);
-                glutKeyboardFunc(keyboard);
-                glewInit();
-                
-                //Initialize Projection Matrix
-                glMatrixMode( GL_PROJECTION );
-                glLoadIdentity();
-                //Initialize Modelview Matrix
-                glMatrixMode( GL_MODELVIEW );
-                glLoadIdentity();
-                //Initialize clear color to black
-                glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
 
-                //Check for errors
-                if (!GLEW_VERSION_2_1) {
-                    throw std::runtime_error("OpenGL 2.1 not available");
+            // Calls update_game_state every 1/FPS seconds
+            // Calls render as often as possible
+            void main_loop() {
+                auto a = std::chrono::high_resolution_clock::now();
+               while (!ui_controller.shouldClose()) {
+                    auto b = std::chrono::high_resolution_clock::now();
+                    auto delta_time = std::chrono::duration_cast<std::chrono::milliseconds>(b - a).count();
+                    if(delta_time >= 1000 / FPS) {
+                        a = b;
+                        update_game_state();
+                    }
+                    ui_controller.render();
+                    glfwPollEvents();
+                    
                 }
-                if (glGetError() != GL_NO_ERROR) {
-                    std::string error_message = "OpenGL error: " + std::string((const char*)gluErrorString(glGetError()));
-                    throw std::runtime_error(error_message);
-                }
-
-                // Start GLUT main loop
-                glutTimerFunc( 1000 / FPS, main_loop, 0 );
-                glutMainLoop();
             }
-
-            static void main_loop(int val) {
-                // Frame logic
-                update_game_state();
-                render();
-
-                // Call main loop again
-                glutTimerFunc( 1000 / FPS, main_loop, val );
-            }
-            // Render the current state of the simulation
-            // PRECONDITION:
-            //     - OpenGL context must be initialized
-            //     - Active modelview matrix
-            static void render(){
-                //Clear color buffer
-                glClear( GL_COLOR_BUFFER_BIT );
-
-                //Render quad
-                glBegin( GL_QUADS );
-                    glVertex2f( -0.5f, -0.5f );
-                    glVertex2f(  0.5f, -0.5f );
-                    glVertex2f(  0.5f,  0.5f );
-                    glVertex2f( -0.5f,  0.5f );
-                glEnd();
-
-                //Update screen
-                glutSwapBuffers();
-            }
-
-
+        
             // Called every frame
             static void update_game_state(){}
 
-            // Called when a key is pressed
-            static void keyboard(unsigned char key, int x, int y){}
-
             PointManager point_manager;
+            UIController ui_controller;
     };
 }
